f6333b26e4482995786a8983d804bf1b76c922e9
Miguel <miguelmartinfernandez94@gmail.com>
2022-04-20 18:17:59 +0200

    20/04 18:18

--- RelacionEjercicios/src/main/java/ejercicio4/Vehiculo.java
+++ RelacionEjercicios/src/main/java/ejercicio4/Vehiculo.java
@@ -1,8 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package ejercicio4;
+
+import java.util.Random;
+
 
 /**
  *
@@ -20,7 +20,10 @@ public abstract class Vehiculo {
     }
 
     public Vehiculo() {
-        
+        String []marcas={"Ford", "Fiat","Toyota","Tesla","Reanult"};
+        Random ale=new Random();
+       this.marca=marcas[ale.nextInt(marcas.length)];
+                
     }
 
     public String getMatricula() {
