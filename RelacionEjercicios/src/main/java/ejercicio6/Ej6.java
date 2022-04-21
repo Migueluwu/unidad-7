d6404e4b185db5a39103d7379283b7f5fa0bd113
Miguel <miguelmartinfernandez94@gmail.com>
2022-04-21 12:17:57 +0200

    21/4 12:17

--- RelacionEjercicios/src/main/java/ejercicio6/Ej6.java
+++ RelacionEjercicios/src/main/java/ejercicio6/Ej6.java
@@ -23,30 +23,33 @@ public class Ej6 {
         System.out.println("Leyendo el fichero: " + idFichero);
         String linea;
         String[] tokens;
-        int posicion=0;
+        int posicion = 0;
+        int numlinea = 0;
         // Inicialización del flujo "datosFichero" en función del archivo llamado "idFichero"
         // Estructura try-with-resources. Permite cerrar los recursos una vez finalizadas
         // las operaciones con el archivo
-        try (Scanner datosFichero = new Scanner(new File(idFichero), "UTF-8")) {
+        try ( Scanner datosFichero = new Scanner(new File(idFichero), "UTF-8")) {
             while (datosFichero.hasNextLine()) {
-                posicion=-1;
-                linea=datosFichero.nextLine();
-                tokens=linea.split(";");
-                for(int i=0;i< tokens.length;i++){
-                    
-                    if(tokens[i].equalsIgnoreCase("w")&&tokens[i+1].equalsIgnoreCase("e")
-                            && tokens[i+2].equalsIgnoreCase("b")){
-                        posicion=i;
+                posicion = -1;
+                linea = datosFichero.nextLine();
+                tokens = linea.split(";");
+                if (linea.contains("w") || linea.contains("W")) {
+                    for (int i = 0; i < tokens.length - 2; i++) {
+
+                        if (tokens[i].equalsIgnoreCase("w") && tokens[i + 1].equalsIgnoreCase("e")
+                                && tokens[i + 2].equalsIgnoreCase("b")) {
+                            posicion = i;
+                        }
                     }
                 }
-                 System.out.println(posicion);
+                System.out.println("Numero de linea " + numlinea + " en la posicion " + posicion);
+                numlinea++;
             }
-           
-        
-        }catch (FileNotFoundException e) {
+
+        } catch (FileNotFoundException e) {
             System.out.println(e.getMessage());
         }
-        
+
     }
-    
+
 }
---
+++ RelacionEjercicios/src/main/java/ejercicio7/Ej7.java
@@ -0,0 +1,65 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio7;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+/**
+ *
+ * @author miguel
+ */
+public class Ej7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws FileNotFoundException {
+        // TODO code application logic here
+        String idFichero = "ejercicio4.txt";
+        System.out.println("Leyendo el fichero: " + idFichero);
+        ArrayList<String> listaVehiculos = new ArrayList<>();
+        ArrayList<Vehiculo> lista = new ArrayList<>();
+        String linea;
+        String[] tokens;
+
+        int contador = 0;
+        try ( Scanner datosFichero = new Scanner(new File(idFichero), "UTF-8")) {
+            while (datosFichero.hasNextLine()) {
+                linea = datosFichero.nextLine();
+                listaVehiculos.add(linea);
+            }
+            for (String aux : listaVehiculos) {
+                tokens = aux.split(":");
+                if (tokens[0].charAt(0) == '1') {
+                    lista.add(
+                            new Turismo(Integer.parseInt(tokens[3]), tokens[0], tokens[1], tokens[2]));
+                }
+                if (tokens[0].charAt(0) == '2') {
+                    lista.add(
+                            new Deportivo(Integer.parseInt(tokens[3]), tokens[0], tokens[1], tokens[2]));
+                }
+                if (tokens[0].charAt(0) == '3') {
+                    lista.add(
+                            new Furgoneta(Integer.parseInt(tokens[3]), tokens[0], tokens[1], tokens[2]));
+                }
+
+            }
+            ordenarPorMarca(lista);
+            System.out.println(lista.toString());
+        }
+
+    }
+
+    public static void ordenarPorMarca(ArrayList<Vehiculo> lista) {
+        Collections.sort(lista,
+                (Vehiculo v1, Vehiculo v2) -> v1.getMarca().compareTo(v2.getMarca()));
+    }
+
+}
