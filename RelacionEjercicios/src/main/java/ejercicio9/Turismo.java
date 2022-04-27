0c8c54462ad8cc0dedc5f61b1c85759a5e890bef
Miguel <miguelmartinfernandez94@gmail.com>
2022-04-27 14:44:41 +0200

    27/4 14:44

---
+++ RelacionEjercicios/src/main/java/ejercicio9/Turismo.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicio7;
+
+import ejercicio4.*;
+
+/**
+ *
+ * @author miguel
+ */
+public class Turismo extends Vehiculo{
+    private int cilindradas;
+
+    public Turismo(int cilindradas, String matricula, String marca, String color) {
+        super(matricula, marca, color);
+        this.cilindradas = cilindradas;
+    }
+
+    public Turismo() {
+    }
+
+    public int getCilindradas() {
+        return cilindradas;
+    }
+
+    public void setCilindradas(int cilindradas) {
+        this.cilindradas = cilindradas;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString()+ ":" + cilindradas ;
+    }
+    
+}
---
+++ RelacionEjercicios/src/main/java/ejercicio9/Vehiculo.java
@@ -0,0 +1,59 @@
+
+package ejercicio7;
+
+import ejercicio4.*;
+import java.util.Random;
+
+
+/**
+ *
+ * @author miguel
+ */
+public abstract class Vehiculo {
+    private String matricula;
+    private String marca;
+    private String color;
+
+    public Vehiculo(String matricula, String marca, String color) {
+        this.matricula = matricula;
+        this.marca = marca;
+        this.color = color;
+    }
+
+    public Vehiculo() {
+        String []marcas={"Ford", "Fiat","Toyota","Tesla","Reanult"};
+        Random ale=new Random();
+       this.marca=marcas[ale.nextInt(marcas.length)];
+                
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return  matricula + ":" + marca + ":" + color;
+    }
+    
+}
